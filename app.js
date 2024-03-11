0ae439aeb9ccf8fe720d6e5cce4e06d5958a392b
annociu <dosoem78@gmail.com>
2024-03-11 20:49:11 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = require("./routes");
+const app = express();
+
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
+
+app.use(router);
+
+app.listen(3000, console.log("http://localhost:3000"));
+
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const home = require("./home");
+const about = require("./about");
+
+router.use("/", home);
+router.use("/about", about);
+
+module.exports = router;
+
